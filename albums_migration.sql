1a2b1f4c0d7c87ff741a67f6d24a0d46a982d2dc
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-26 16:03:25 -0600

    cleaned up code

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE albums(
     artist VARCHAR (30) NOT NULL,
     name VARCHAR (30) NOT NULL,
     release_date DATE,
-    sales DECIMAL (11,2),
+    sales DECIMAL (1,2),
     genre VARCHAR (15),
     PRIMARY KEY (id)
 );
