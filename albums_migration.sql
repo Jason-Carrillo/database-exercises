b3d0962bb6b9251772c0d798d7e093107890105b
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-26 14:35:25 -0600

    added drop database

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,8 +1,10 @@
+DROP DATABASE IF EXISTS codeup_test_db;
+USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id int(10) unsigned NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (30) NOT NULL ,
-    name VARCHAR (30) NOT NULL ,
+    artist VARCHAR (30) NOT NULL,
+    name VARCHAR (30) NOT NULL,
     release_date DATE,
     sales DECIMAL (11,2),
     genre VARCHAR (15),
