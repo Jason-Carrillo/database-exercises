3818f55c9ca6d0a44bb767f034973bef5a8982d5
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 12:45:45 -0600

    completed exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
 SELECT 'Released after 1991' AS 'Albums';
-SELECT name FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'Disco Genre' AS 'Albums';
-SELECT name FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 
 SELECT 'by Whitney Houston' AS 'Albums';
-SELECT name FROM albums WHERE artist LIKE 'Whitney Houston%';
+DELETE FROM albums WHERE artist LIKE 'Whitney Houston%';
