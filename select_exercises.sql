1d8ecac4f906b94fe78b1adffdc371195dd692ed
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 10:48:57 -0600

    searched for albums released after 1990 with caption

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,3 +8,6 @@ SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club 
 
 SELECT 'Nevermind' AS 'Nirvana';
 SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Released after 1990' AS 'Albums';
+SELECT * FROM albums WHERE release_date >= 1990;
