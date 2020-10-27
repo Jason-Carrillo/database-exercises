df00edada13dc5be2d217f22e4067554dc5b7f2a
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 10:50:26 -0600

    searched for albums with less than 20 million in sales with caption

--- select_exercises.sql
+++ select_exercises.sql
@@ -11,3 +11,6 @@ SELECT genre FROM albums WHERE name = 'Nevermind';
 
 SELECT 'Released after 1990' AS 'Albums';
 SELECT * FROM albums WHERE release_date >= 1990;
+
+SELECT 'With less than 20 Million in sales' AS 'Albums';
+SELECT * FROM albums WHERE sales < 20;
