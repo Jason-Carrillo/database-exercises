07d6f83af7383e0603fcf19d488a71659ac9f396
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 11:03:28 -0600

    added all albums by michael jackson with caption

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,3 +5,6 @@ SELECT * FROM albums;
 
 SELECT 'Released before 1980' AS 'Albums';
 SELECT * FROM albums WHERE release_date < 1980;
+
+SELECT 'Michael Jackson' AS 'Albums by';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
