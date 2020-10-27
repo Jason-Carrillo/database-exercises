2d1bf51dd02ce423d125207a5f542fb5f1c66b93
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 11:26:48 -0600

    Added selectors after each update

--- update_exercises.sql
+++ update_exercises.sql
@@ -16,11 +16,17 @@ UPDATE albums
 SET release_date = release_date-80
 WHERE release_date < 1980;
 
+SELECT 'Released before 1980' AS 'Albums';
+SELECT * FROM albums WHERE release_date < 1980;
+
 SELECT 'Michael Jackson' AS 'Albums by';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
+
+SELECT 'Michael Jackson' AS 'Albums by';
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
 
 -- Change 'Michael Jackson' to 'Peter Jackson'
