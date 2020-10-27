3af52d3ae9cc4216ff9cc067519f8db85879aee1
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 10:47:02 -0600

    searched for nivana nevermind genres

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,6 @@ SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT 'Release' AS 'Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'Nevermind' AS 'Nirvana';
+SELECT genre FROM albums WHERE name = 'Nevermind';
