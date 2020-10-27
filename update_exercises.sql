63df38615313df9f3ee4ea6fa9309ab3b308cb08
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 11:23:30 -0600

    Removed 80 years from all albums released before 1980

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,8 +3,18 @@ USE codeup_test_db;
 SELECT 'Albums' AS 'List of all';
 SELECT * FROM albums;
 
+UPDATE albums
+SET sales = sales * 10;
+
 SELECT 'Released before 1980' AS 'Albums';
 SELECT * FROM albums WHERE release_date < 1980;
 
+UPDATE albums
+SET release_date = release_date-80
+WHERE release_date < 1980;
+
 SELECT 'Michael Jackson' AS 'Albums by';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+-- Move all the albums before 1980 back to the 1800s.
+-- Change 'Michael Jackson' to 'Peter Jackson'
