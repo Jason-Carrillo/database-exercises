ddfbf2a59b6935508015762f21b1848c21874840
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 10:45:47 -0600

    added pepper original release date with caption

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,3 +2,6 @@ USE codeup_test_db;
 
 SELECT 'Albums' AS 'Pink Floyd';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'Release' AS 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
