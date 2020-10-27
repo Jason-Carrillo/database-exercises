a7b0b88961c3d0724be5af7728c21661259d58fc
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-27 11:25:02 -0600

    changed michael jackson to peter jackson

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,6 +6,9 @@ SELECT * FROM albums;
 UPDATE albums
 SET sales = sales * 10;
 
+SELECT 'Albums' AS 'List of all';
+SELECT * FROM albums;
+
 SELECT 'Released before 1980' AS 'Albums';
 SELECT * FROM albums WHERE release_date < 1980;
 
@@ -16,5 +19,8 @@ WHERE release_date < 1980;
 SELECT 'Michael Jackson' AS 'Albums by';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 
--- Move all the albums before 1980 back to the 1800s.
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
 -- Change 'Michael Jackson' to 'Peter Jackson'
