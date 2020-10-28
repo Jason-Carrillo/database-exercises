5a96a1436b75f4d504323c83cc2031e511c8290a
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-28 10:43:10 -0600

    searched for employee that is the oldest born on christmas day, and newest hire

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -37,10 +37,11 @@ select *
 # AND hire_date > '1990-00-00';
 # # AND hire_date < '2000-00-00';
 
-# select *
-# from employees
-# WHERE hire_date LIKE '199%'
-# AND birth_date LIKE '%-12-25';
+select *
+from employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
 # select *
 # from employees
