71fbc4bee8e0dd5a6340d9940b320b1495eb08e2
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-28 09:30:51 -0600

    searched for all employees that start with an E

--- src/where_exercises.sql
+++ src/where_exercises.sql
@@ -1,6 +1,10 @@
 USE employees_db;
 
+# select *
+# from employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, birth_date;
+
 select *
 from employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, birth_date;
+WHERE last_name LIKE 'E%';
