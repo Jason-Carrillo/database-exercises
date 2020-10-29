0d9e348a196bc2ed9b711f188a74cffdb1e04e80
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-29 15:52:39 -0600

    added all managers of departments

--- src/join_exercises.sql
+++ src/join_exercises.sql
@@ -2,7 +2,7 @@ USE employees;
 
 DESCRIBE employees;
 
-SELECT d.dept_name,
+SELECT dept_name
 FROM employees e
 JOIN dept_emp de
     ON e.emp_no = de.emp_no
@@ -10,6 +10,22 @@ JOIN departments d
     on d.dept_no = de.dept_no;
 # WHERE e.first_name = 'Yuchang';
 # AND e.last_name = 'Weedman';
+
+SELECT dept_name, e.first_name, e.last_name
+FROM departments d
+    JOIN dept_manager dm
+        on d.dept_no = dm.dept_no
+    JOIN employees e
+        ON e.emp_no = dm.emp_no
+WHERE dm.to_date = '9999-01-01';
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01'
+    );
 
 
 SELECT * FROM
@@ -26,3 +42,13 @@ SELECT * FROM
               departments;
 
 DESCRIBE departments;
+
+show tables;
+
+SELECT * FROM dept_manager;
+
+describe salaries;
+
+SELECT * FROM titles;
+
+describe titles;
