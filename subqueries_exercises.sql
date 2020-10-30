7897c76400043d4d24d01f90b8ea9444c52dbb84
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-30 09:54:00 -0600

    searched for Aamod employee

---
+++ subqueries_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS 'Full Name'
+FROM employees
+    WHERE hire_date = (
+SELECT hire_date
+FROM employees
+WHERE emp_no = 101010);
+
+SELECT title, emp_no
+FROM titles
+WHERE emp_no IN (SELECT emp_no
+                FROM employees
+                WHERE first_name = 'Aamod');
+
+SELECT emp_no
+FROM employees
+WHERE first_name = 'Aamod';
