7395b0a51fe2c44d2a2df885cae218ed9d18e95e
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-30 10:03:09 -0600

    searched for all female managers

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,13 +7,22 @@ SELECT hire_date
 FROM employees
 WHERE emp_no = 101010);
 
-SELECT title, emp_no
+
+
+SELECT title, COUNT(title)
 FROM titles
 WHERE emp_no IN (SELECT emp_no
                 FROM employees
                 WHERE first_name = 'Aamod')
-ORDER BY title;
+GROUP BY title;
 
 SELECT emp_no
 FROM employees
 WHERE first_name = 'Aamod';
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) as 'Full Name', gender
+FROM employees
+WHERE emp_no IN (SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01')
+AND gender = 'f';
